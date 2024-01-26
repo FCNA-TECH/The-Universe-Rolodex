c95ee4147232d06be5d58bd8082fa8da329c4ca4
Ferdinand <97032094+FCNA-TECH@users.noreply.github.com>
2024-01-26 20:18:36 +0000

    Create CelestialBody.cs

---
+++ CelestialBody.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
+
+class CelestialBody
+{
+    protected string Name;
+    protected double Mass;
+    public string GetName()
+    {
+        return Name;
+    }
+ 
+    public double GetMass() 
+    { 
+        return Mass; 
+    }
+}
