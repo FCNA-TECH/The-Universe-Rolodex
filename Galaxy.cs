a3337d700210e1903a1ef9e7bb4264fa3d72a753
Ferdinand <97032094+FCNA-TECH@users.noreply.github.com>
2024-01-26 20:20:32 +0000

    Create Galaxy.cs

---
+++ Galaxy.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
+
+class Galaxy : CelestialBody                                // Inherits name and mass from the celestial body super class but mass is never used
+{
+    private double Magnitude;
+    private List<Star> Stars = new List<Star>();
+ 
+    public Galaxy(string varName, double varMagnitude)      // Constructor
+    { 
+        Name = varName;
+        Magnitude = varMagnitude;
+    }
+ 
+    public void AddStar(Star varStar)
+    {
+        Stars.Add(varStar);
+    }
+    public void Print()
+    {
+        Console.WriteLine("Galaxy: {0} has a magnitude of {1}", Name, Magnitude);
+        foreach (Star item in Stars)
+        {
+            item.Print();
+        }
+    }
+}
