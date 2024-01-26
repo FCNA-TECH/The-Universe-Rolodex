3947e3c62e2aecc2a5dd2da9112a8f5624f47d4f
Ferdinand <97032094+FCNA-TECH@users.noreply.github.com>
2024-01-26 20:27:18 +0000

    Create Moon.cs

---
+++ Moon.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
+
+class Moon : CelestialBody                            // Inherits name and Mass from Celestial Body
+{
+    private double Speed;  
+ 
+    public Moon (string varName, double varSpeed) 
+    {
+        Name = varName;
+        Speed = varSpeed;
+    }
+ 
+    public void Print()
+    {
+        Console.WriteLine("\t\t\t    Moon: {0} has a speed of {1}m/s", Name, Speed);
+    }
+}
