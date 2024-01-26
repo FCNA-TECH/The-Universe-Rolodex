f1a97dd86633f6cc7c6985ee6bc1957288fda2df
Ferdinand <97032094+FCNA-TECH@users.noreply.github.com>
2024-01-26 20:28:49 +0000

    Update CelestialBody.cs

--- CelestialBody.cs
+++ CelestialBody.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks
+using System.Threading.Tasks;
 
 class CelestialBody                        // This is the super class that all other classses will inherit name and mass properties from
 {
